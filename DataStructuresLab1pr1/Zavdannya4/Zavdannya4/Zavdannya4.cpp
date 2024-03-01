e68285a6832152703fa89d6bc0d7cd48cc1fc34c
Par722 <sasha.sydda@gmail.com>
2024-03-01 13:32:10 +0200

    no message

---
+++ DataStructuresLab1pr1/Zavdannya4/Zavdannya4/Zavdannya4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter an integer (at least 2): ";
+    cin >> n;
+
+    int i = 2;
+    while (n % i != 0) {
+        i++;
+    }
+
+    cout << "The smallest natural divisor other than 1: " << i << endl;
+
+    return 0;
+}
